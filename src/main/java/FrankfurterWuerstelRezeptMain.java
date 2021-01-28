57b4b432c372daf3c46700da5cec4434988852a4
fhochleitner <felix.hochleitner@outlook.com>
2021-01-28 11:47:56 +0100

    initial commit

---
+++ src/main/java/FrankfurterWuerstelRezeptMain.java
@@ -0,0 +1,16 @@
+public class FrankfurterWuerstelRezeptMain
+{
+    public static void main(String[] args) {
+        cookWiaschtl();
+        enjoyWiaschtl();
+    }
+
+    private static void cookWiaschtl(){
+        System.out.println("Heast, stel das Wasser auf, hau de Wiaschtl eine und brings zum kochen. Wenns koch, bist fertig");
+    }
+
+    private static void enjoyWiaschtl(){
+        System.out.println("Haus da Eini ind Figur. Ned aufs Bier dazua vagessen");
+    }
+
+}
